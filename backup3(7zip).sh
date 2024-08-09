352ed6e406fd97dc585215d7536bcf681f1967d1
greksw <greksw@gmail.com>
2024-08-09 18:06:41 +0300

    Update and rename backup2(7zip).sh to backup3(7zip).sh
    Исправление скрипта в части создания архива сразу на точке монтирования 2

--- backup3(7zip).sh
+++ backup3(7zip).sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Пути к лог-файлу и точкам монтирования
-LOG_FILE="/mnt/srv-ts5-ad.log"
-MOUNT_POINT1="/mnt/srv-ts5-ad"
-MOUNT_POINT2="/mnt/srv-ts5-ad-tr"
+LOG_FILE="/mnt/srv-ts5.log"
+MOUNT_POINT1="/mnt/srv-ts5"
+MOUNT_POINT2="/mnt/srv-ts5-tr"
 
 # Telegram Bot API параметры
 TOKEN="1115999333:dddmuRf8mAfEpSjXZLdffghhlQu0jY"
 CHAT_ID="116667787"
 
 # Название скрипта для уведомлений
-SCRIPT_NAME="Резервное копирование srv-ts5_c_ad:\*"
+SCRIPT_NAME="Резервное копирование srv-ts5_d:\*"
 
 # Функция для записи логов и отправки уведомлений
 log_and_notify() {
@@ -27,8 +27,8 @@ HOST2="192.168.2.200"
 ZIP_PASSWORD="1234567890"
 
 # Создание имени архива на основе текущей даты
-DATE=$(date '+%Y-%m-%d-%H:-%M:-%S')
-ARCHIVE_NAME="srv-ts5-ad_backup_$DATE.7z"
+DATE=$(date '+%Y-%m-%d')
+ARCHIVE_NAME="srv-ts5_backup_$DATE.7z"
 
 # Проверка доступности хостов
 ping -c 1 $HOST1 > /dev/null 2>&1
@@ -42,11 +42,11 @@ if [ $HOST1_STATUS -eq 0 ] && [ $HOST2_STATUS -eq 0 ]; then
     log_and_notify "✅ Оба хоста доступны, монтируем шары..."
 
     # Монтируем шару с первого хоста
-    sudo mount -t cifs //192.168.2.13/c$/Users/use1 $MOUNT_POINT1 -o username=admin1,password=123,domain=fm.loc,iocharset=utf8,file_mode=0777,dir_mode=0777
+    sudo mount -t cifs //192.168.2.13/d$/test $MOUNT_POINT1 -o username=user1,password=123,domain=test.loc,iocharset=utf8,file_mode=0777,dir_mode=0777
     MOUNT1_STATUS=$?
 
     # Монтируем шару со второго хоста
-    sudo mount -t cifs //192.168.2.200/srv-win/srv-ts $MOUNT_POINT2 -o username=admin2,password=123,domain=workgroup,iocharset=utf8,file_mode=0777,dir_mode=0777
+    sudo mount -t cifs //192.168.2.200/srv-win/srv-ts5 $MOUNT_POINT2 -o username=user2,password=123,domain=workgroup,iocharset=utf8,file_mode=0777,dir_mode=0777
     MOUNT2_STATUS=$?
 
     # Если монтирование прошло успешно
@@ -54,23 +54,13 @@ if [ $HOST1_STATUS -eq 0 ] && [ $HOST2_STATUS -eq 0 ]; then
         log_and_notify "✅ Шары успешно примонтированы, выполняем архивирование..."
 
         # Проверка наличия папок перед архивированием
-        if [ -d "$MOUNT_POINT1/Downloads" ] && [ -d "$MOUNT_POINT1/Documents" ] && [ -d "$MOUNT_POINT1/Desktop" ]; then
-            # Архивируем только папки Загрузки, Документы и Рабочий стол с использованием 7zip
-            7z a -p$ZIP_PASSWORD "/mnt/$ARCHIVE_NAME" "$MOUNT_POINT1/Downloads" "$MOUNT_POINT1/Documents" "$MOUNT_POINT1/Desktop"
+        if [ -d "$MOUNT_POINT1/Загрузки" ] && [ -d "$MOUNT_POINT1/Документы" ] && [ -d "$MOUNT_POINT1/Рабочий стол" ]; then
+            # Архивируем только папки Загрузки, Документы и Рабочий стол сразу в шару второго хоста
+            7z a -p$ZIP_PASSWORD "$MOUNT_POINT2/$ARCHIVE_NAME" "$MOUNT_POINT1/Загрузки" "$MOUNT_POINT1/Документы" "$MOUNT_POINT1/Рабочий стол"
             ZIP_STATUS=$?
 
             if [ $ZIP_STATUS -eq 0 ]; then
-                log_and_notify "✅ Архивирование успешно завершено: $ARCHIVE_NAME."
-
-                # Копирование архива на второй хост
-                cp /mnt/$ARCHIVE_NAME $MOUNT_POINT2/
-                CP_STATUS=$?
-
-                if [ $CP_STATUS -eq 0 ]; then
-                    log_and_notify "✅ Архив успешно скопирован на второй хост."
-                else
-                    log_and_notify "❌ Ошибка при копировании архива на второй хост."
-                fi
+                log_and_notify "✅ Архивирование успешно завершено и файл сохранен в $MOUNT_POINT2/$ARCHIVE_NAME."
             else
                 log_and_notify "❌ Ошибка при создании архива."
             fi
@@ -90,7 +80,7 @@ if [ $HOST1_STATUS -eq 0 ] && [ $HOST2_STATUS -eq 0 ]; then
         if mount | grep $MOUNT_POINT1 > /dev/null; then
             sudo umount $MOUNT_POINT1
         fi
- 
+        
         if mount | grep $MOUNT_POINT2 > /dev/null; then
             sudo umount $MOUNT_POINT2
         fi
